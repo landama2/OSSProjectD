3b8e8818f1da32ff0cb835c56c11ef355c1db5fb
landama2 <landama2@users.noreply.github.com>
2015-12-29 00:33:43 +0100

    Main from test_cfg_parser.c to main.c

--- main.c
+++ main.c
@@ -11,7 +11,10 @@
 #include <unistd.h>
 #include <stdbool.h>
 
+#include "route_cfg_parser.h"
+
 bool verbosity = false;
+
 void verbPrintf(const char *message) {
 
     if (verbosity)
@@ -22,37 +25,29 @@ void verbPrintf(const char *message) {
     // Do nothing
 }
 
-int main(int argc, char *argv[]) {
-    if (argc == 1) /* argc should be 2 for correct execution */{
-        /* We print argv[0] assuming it is the program name */
-        verbPrintf("ID of this node must be specified!");
-//        printf( "usage: %s filename", argv[0] );
-    } else if (argc == 2) {
-        printf("usage: %s filename\n", argv[0]);
-        printf("Number of this node is: %s \n", argv[1]);
-    } else {
-        struct sockaddr_in si_me, si_other;
-        return 0;
-        // We assume argv[1] is a filename to open
-        FILE *file = fopen(argv[1], "r");
-
-        /* fopen returns 0, the NULL pointer, on failure */
-        if (file == 0) {
-            printf("Could not open file\n");
-        } else {
-            int x;
-            /* read one character at a time from file, stopping at EOF, which
-               indicates the end of the file.  Note that the idiom of "assign
-               to a variable, check the value" used below works because
-               the assignment statement evaluates to the value assigned. */
-            while ((x = fgetc(file)) != EOF) {
-                printf("%c", x);
-            }
-            fclose(file);
-        }
+int main(int argc, char ** argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Not enough arguments\nUsage: %s <ID> [ <cfg-file-name> ]\n", argv[0]);
+        return -1;
     }
-    // //
-    printf("Test commit2");
-    printf("Misa tady byla a upravila si mail a jeste si ho dala private!!");
-    return 0;
+    int localId = atoi(argv[1]);
+    int connectionCount = 100;
+    int localPort;
+    TConnection connections[connectionCount];
+    printf("\n%s\n",argv[2]);
+    int result = parseRouteConfiguration((argc>2)?argv[2]:NULL, localId, &localPort, &connectionCount, connections);
+    if (result) {
+        printf("OK, local port: %d\n", localPort);
+
+        if (connectionCount > 0) {
+            int i;
+            for (i=0; i<connectionCount; i++) {
+                printf("Connection to node %d at %s%s%d\n", connections[i].id, connections[i].ip_address, (connections[i].ip_address[0])?":":"port ", connections[i].port);
+            }
+        } else {
+            printf("No connections from this node\n");
+        }
+    } else {
+        printf("ERROR\n");
+    }
 }
